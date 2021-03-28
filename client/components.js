4167f9831d91c71c6ebf20ca6098d99db219c8c5
Jakob <e11809618@student.tuwien.ac.at>
2021-03-28 13:42:58 +0200

    work on sway

--- client/components.js
+++ client/components.js
@@ -73,38 +73,53 @@ export class SimpleGLTFModel extends Component {
 	}
 }
 
-export class SimpleGunModel extends SimpleGLTFModel{
+export class SimpleGunModel extends Component{
     constructor(gameObject, path, params){
-        super(gameObject, path, params);
+        super(gameObject);
 
-        //this.targetRotation  = new THREE.Quaternion();
-        //this.oldRotation     = new THREE.Quaternion();
+        let rotation = params.rotation ? params.rotation : new THREE.Vector3();
+        let position = params.position ? params.position : new THREE.Vector3();
+        let scale    = params.scale    ? params.scale    : new THREE.Vector3(1,1,1);
+
+        (async () => {
+            const loader = new GLTFLoader();
+            const gltf = await new Promise((resolve, reject) => {
+                loader.load(path, data => resolve(data), null, reject);
+            });
+            this.model = gltf.scene;
+            this.model.position.copy(position);
+            this.model.rotateX(rotation.x);
+            this.model.rotateY(rotation.y);
+            this.model.rotateZ(rotation.z);
+            this.model.scale.copy(scale)
+            this.gameObject.transform.add(this.model)
+            
+            this.old = new THREE.Vector3();
+            this.tmp = new THREE.Vector3();
+            this.tmp2 = new THREE.Vector3();
+            this.model.getWorldPosition(this.old);
+        })();
 
         this.targetPosition = new THREE.Vector3();
-        this.oldPosition = new THREE.Vector3();
+        this.oldPosition    = new THREE.Vector3();
+
+        this.muzzlePosition = position;
 
         this.fire = undefined;
         this.s = 1.0;
         this.factor = 1.0;
 
+        this.time = 0;
+        
+
         this.gameObject.subscribe("fire", (event) => {
 
             if (this.fire != undefined) {
-                //this.model.quaternion.copy(this.oldRotation);
                 this.model.position.copy(this.oldPosition);
             }
 
             this.fire = true;
             this.s = 0.0;
-
-            /*
-            let e = new THREE.Euler(
-                this.model.rotation.x + 0.5,
-                this.model.rotation.y,
-                this.model.rotation.z,
-            );
-            this.targetRotation.setFromEuler(e);
-            */
 
             this.targetPosition.set(
                 this.model.position.x,
@@ -112,16 +127,16 @@ export class SimpleGunModel extends SimpleGLTFModel{
                 this.model.position.z + 0.1
             )
             
-            //this.oldRotation.copy(this.model.quaternion);
             this.oldPosition.copy(this.model.position);
         })
     }
 
     update(dt){
+        this.time += dt;
+
         if (this.model){
             if (this.fire && this.s <= 1.0){
 
-                //this.model.quaternion.slerp(this.targetRotation, this.s)
                 this.model.position.lerp(this.targetPosition, this.s);
                 this.s += 7 * dt;
 
@@ -131,12 +146,24 @@ export class SimpleGunModel extends SimpleGLTFModel{
                 }
             }
             
-            
             if (this.fire == false && this.s <= 1.0){
-                //this.model.quaternion.slerp(this.oldRotation, this.s)
                 this.model.position.lerp(this.oldPosition, this.s);
                 this.s += 7 * dt;
             }
+
+            this.model.getWorldPosition(this.tmp);
+            this.tmp2.subVectors(this.tmp, this.old);
+            this.old.copy(this.tmp);
+
+           
+            
+            let x = this.muzzlePosition.x, y = this.muzzlePosition.y;
+            let vsa = 0.02, hsa = 0.02;
+            let sp = 1.0;
+            y += vsa * Math.sin((sp)     * this.time);
+            x += hsa * Math.sin((sp * 2) * this.time);
+            this.model.position.setX(x + this.tmp2.x * 0.5);
+            this.model.position.setY(y + this.tmp2.y * 0.5);
             
         }
     }
